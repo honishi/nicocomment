af7cec05d1ca988d897b803de47d5dbc7229a692
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2013-11-25 15:12:03 +0900

    Added submodules.

--- nicocomment.sh
+++ nicocomment.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-export LD_LIBRARY_PATH=/home/honishi/local/openssl-1.0.0k/lib
+# export LD_LIBRARY_PATH=/home/honishi/local/openssl-1.0.0k/lib
+LD_LIBRARY_PATH=/home/honishi/local/openssl-1.0.0k/lib
 
 ./nicocomment.py
 
